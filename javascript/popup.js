cdb853b50ff5bacb9e92778d91092921e152c5e9
hmaruyama <maruyama@val.co.jp>
2015-04-18 15:44:55 +0900

    【fix_request_roote】配列の表記を治してみた

--- javascript/popup.js
+++ javascript/popup.js
@@ -11,7 +11,7 @@ ls = {
 };
 
 var path = '/search/course/extreme'
-var via_list = localStorage['via'] ? ls['dp']['code'] + ":" + ls['via']['code'] + ":" +  ls['ar']['code'] : ls['dp']['code'] + ":" +  ls['ar']['code'];
+var via_list = localStorage.via ? ls.dp.code + ":" + ls.via.code + ":" +  ls.ar.code : ls.dp.code + ":" +  ls.ar.code;
 var params = {
   key: key(),
   viaList: via_list,
@@ -22,7 +22,7 @@ var courseList = createCourseList(getResponse(path, params));
 console.log(courseList);
 
 onload = function() {
-  if (localStorage['dp'] && localStorage['ar']) {
+  if (localStorage.dp && localStorage.ar) {
     document.getElementById('station').textContent = showViaList();
     var now = new Date();
     // var now = moment();
@@ -37,21 +37,21 @@ onload = function() {
       var a = document.createElement('a');
       var target = a.setAttribute('target', 'blank');
       var minute1 = Number(courseList[i].dp_datetime.getMinutes() - (Math.floor(courseList[i].dp_datetime.getMinutes() / 10) * 10));
-      // var roote = "http://roote.ekispert.net/result?dep_code=" + ls['dp']['code'] + "&arr_code=" + ls['ar']['code'] + "&submit.x=108&submit.y=16&dep=" + ls['dp']['name'] + "&arr=" + ls['ar']['name'] + "&via1=" + ls['via']['name'] + "&yyyymm=" + courseList[i].dp_datetime.getFullYear().toString() + doubleDigits((courseList[i].dp_datetime.getMonth() + 1)) + "&day=" + courseList[i].dp_datetime.getDate() + "&hour=" + courseList[i].dp_datetime.getHours() + "&minute10=" + Math.floor(courseList[i].dp_datetime.getMinutes() / 10) + "&minute1=" + minute1 + "&type=dep&sort=time&transfer=2&surcharge=3&shinkansen=true&express=true&local=true&highway=true&plane=true&connect=true&liner=true&sleep=true&ship=true";
+      // var roote = "http://roote.ekispert.net/result?dep_code=" + ls.dp.code + "&arr_code=" + ls.ar.code + "&submit.x=108&submit.y=16&dep=" + ls.dp.name + "&arr=" + ls.ar.name + "&via1=" + ls.via.name + "&yyyymm=" + courseList[i].dp_datetime.getFullYear().toString() + doubleDigits((courseList[i].dp_datetime.getMonth() + 1)) + "&day=" + courseList[i].dp_datetime.getDate() + "&hour=" + courseList[i].dp_datetime.getHours() + "&minute10=" + Math.floor(courseList[i].dp_datetime.getMinutes() / 10) + "&minute1=" + minute1 + "&type=dep&sort=time&transfer=2&surcharge=3&shinkansen=true&express=true&local=true&highway=true&plane=true&connect=true&liner=true&sleep=true&ship=true";
       var path = '/search/course/light';
       var params;
-      if (ls['via']['code']) {
+      if (ls.via.code) {
         params = {
           key: key(),
-          from: ls['dp']['code'],
-          to: ls['ar']['code'],
-          via: ls['via']['code']
+          from: ls.dp.code,
+          to: ls.ar.code,
+          via: ls.via.code
         };
       } else {
         params = {
           key: key(),
-          from: ls['dp']['code'],
-          to: ls['ar']['code']
+          from: ls.dp.code,
+          to: ls.ar.code
         };
       };
 
@@ -76,8 +76,8 @@ function doubleDigits(num) {
 
 // 設定経路の表示
 function showViaList() {
-  var dp_ar_stations = ls['dp']['name'] + " => " + ls['ar']['name'];
-  return ls['via']['name'] ? dp_ar_stations + "  " + ls['via']['name'] + "経由 " : dp_ar_stations;
+  var dp_ar_stations = ls.dp.name + " => " + ls.ar.name;
+  return ls.via.name ? dp_ar_stations + "  " + ls.via.name + "経由 " : dp_ar_stations;
 }
 
 
@@ -105,37 +105,37 @@ function getResponse(path, params) {
 // コースリストの作成
 function createCourseList(response) {
   var course = new Array();
-  response = response['ResultSet']['Course'];
+  response = response.ResultSet.Course;
   if (response instanceof Array) {
     for (var i = 0; i < 3; i++) {
-      if (response[i]['Route']['Line'] instanceof Array) {
-        var length = response[i]['Route']['Line'].length;
+      if (response[i].Route.Line instanceof Array) {
+        var length = response[i].Route.Line.length;
         course[i] = {
-          total_time: Number(response[i]['Route']['timeOnBoard']) + Number(response[i]['Route']['timeOther']) + Number(response[i]['Route']['timeWalk']),
-          dp_datetime: new Date(response[i]['Route']['Line'][0]['DepartureState']['Datetime']['text']),
-          ar_datetime: new Date(response[i]['Route']['Line'][length-1]['ArrivalState']['Datetime']['text'])
+          total_time: Number(response[i].Route.timeOnBoard) + Number(response[i].Route.timeOther) + Number(response[i].Route.timeWalk),
+          dp_datetime: new Date(response[i].Route.Line[0].DepartureState.Datetime.text),
+          ar_datetime: new Date(response[i].Route.Line[length-1].ArrivalState.Datetime.text)
         };
       } else {
         course[i] = {
-          total_time: Number(response[i]['Route']['timeOnBoard']) + Number(response[i]['Route']['timeOther']) + Number(response[i]['Route']['timeWalk']),
-          dp_datetime: new Date(response[i]['Route']['Line']['DepartureState']['Datetime']['text']),
-          ar_datetime: new Date(response[i]['Route']['Line']['ArrivalState']['Datetime']['text'])
+          total_time: Number(response[i].Route.timeOnBoard) + Number(response[i].Route.timeOther) + Number(response[i].Route.timeWalk),
+          dp_datetime: new Date(response[i].Route.Line.DepartureState.Datetime.text),
+          ar_datetime: new Date(response[i].Route.Line.ArrivalState.Datetime.text)
         };
       };
     };
   } else if (response instanceof Object) {
-    if (response['Route']['Line'] instanceof Array) {
-      var length = response['Route']['Line'].length;
+    if (response.Route.Line instanceof Array) {
+      var length = response.Route.Line.length;
       course[0] = {
-        total_time: Number(response['Route']['timeOnBoard']) + Number(response['Route']['timeOther']) + Number(response['Route']['timeWalk']),
-        dp_datetime: new Date(response['Route']['Line'][0]['DepartureState']['Datetime']['text']),
-        ar_datetime: new Date(response['Route']['Line'][length-1]['ArrivalState']['Datetime']['text'])
+        total_time: Number(response.Route.timeOnBoard) + Number(response.Route.timeOther) + Number(response.Route.timeWalk),
+        dp_datetime: new Date(response.Route.Line[0].DepartureState.Datetime.text),
+        ar_datetime: new Date(response.Route.Line[length-1].ArrivalState.Datetime.text)
       };
     } else {
       course[0] = {
-        total_time: Number(response['Route']['timeOnBoard']) + Number(response['Route']['timeOther']) + Number(response['Route']['timeWalk']),
-        dp_datetime: new Date(response['Route']['Line']['DepartureState']['Datetime']['text']),
-        ar_datetime: new Date(response['Route']['Line'][length-1]['ArrivalState']['Datetime']['text'])
+        total_time: Number(response.Route.timeOnBoard) + Number(response.Route.timeOther) + Number(response.Route.timeWalk),
+        dp_datetime: new Date(response.Route.Line.DepartureState.Datetime.text),
+        ar_datetime: new Date(response.Route.Line[length-1].ArrivalState.Datetime.text)
       };
     };
   } else {
